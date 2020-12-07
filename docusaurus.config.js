ce2208adeb7fd1fdd030618f3dacba3eb7cc8350
Cristiano Betta <cristiano@betta.io>
2020-12-07 20:42:54 +0000

    Add initial docs and reference docs

---
+++ docusaurus.config.js
@@ -0,0 +1,117 @@
+module.exports = {
+  title: 'Gr4vy Developer Documentation',
+  tagline: 'Get started with the cloud payment orchestration platform',
+  url: 'https://developer.gr4vy.com',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'gr4vy',
+  projectName: 'developer-documentation',
+  stylesheets: [
+    "https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,400i,700",
+    "/css/code-block-buttons.css"
+  ],
+  themeConfig: {
+    colorMode: {
+      disableSwitch: true,
+    },
+    navbar: {
+      title: 'Gr4vy',
+      logo: {
+        alt: 'Gr4vy Logo',
+        src: 'img/logo.png',
+      },
+      items: [
+        {
+          to: 'guides/',
+          activeBasePath: 'guides',
+          label: 'Guides',
+          position: 'left',
+        },
+        { to: 'reference', label: 'API Reference', position: 'left' },
+        { to: 'blog', label: 'Blog', position: 'left' },
+        {
+          href: 'https://github.com/gr4vy',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'light',
+      links: [
+        {
+          title: 'Docs',
+          items: [
+            {
+              label: 'Style Guide',
+              to: 'guides/',
+            },
+            {
+              label: 'Second Doc',
+              to: 'guides/doc2/',
+            },
+          ],
+        },
+        {
+          title: 'Community',
+          items: [
+            {
+              label: 'Stack Overflow',
+              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+            },
+            {
+              label: 'Discord',
+              href: 'https://discordapp.com/invite/docusaurus',
+            },
+            {
+              label: 'Twitter',
+              href: 'https://twitter.com/docusaurus',
+            },
+          ],
+        },
+        {
+          title: 'More',
+          items: [
+            {
+              label: 'Blog',
+              to: 'blog',
+            },
+            {
+              label: 'GitHub',
+              href: 'https://github.com/facebook/docusaurus',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} Gr4vy, Inc`,
+    },
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          path: 'guides',
+          routeBasePath: 'guides',
+          sidebarPath: require.resolve('./sidebars.js'),
+          editUrl:
+            'https://github.com/gr4vy/developer-documentation/edit/main/guides',
+        },
+        blog: {
+          showReadingTime: true,
+          editUrl:
+            'https://github.com/gr4vy/developer-documentation/edit/main/blog/',
+        },
+        theme: {
+          colorMode: {
+            disableSwitch: true,
+          },
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
+  plugins: ['docusaurus-plugin-sass'],
+};
---
+++ src/pages/reference.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import Layout from '@theme/Layout';
+import { RedocStandalone } from 'redoc';
+import styles from './reference.module.scss';
+
+function Reference() {
+  return (
+    <Layout title="API Reference" >
+      <div className={styles.reference}>
+        <RedocStandalone
+          specUrl="https://raw.githubusercontent.com/gr4vy/gr4vy-openapi/openapi-3.0/openapi.v1.json"
+          options={{
+            disableSearch: true,
+            expandResponses: "200,201,202,204",
+            hideDownloadButton: true,
+            hideSingleRequestSampleTab: true,
+            expandSingleSchemaField: true,
+            jsonSampleExpandLevel: 5,
+            hideSchemaTitles: true,
+            simpleOneOfTypeLabel: true,
+            noAutoAuth: true,
+            onlyRequiredInSamples: true,
+            pathInMiddlePanel: true,
+            requiredPropsFirst: true,
+            scrollYOffset: 60,
+            sortPropsAlphabetically: true,
+            theme: {
+              spacing: {
+                sectionVertical: ({ spacing }) => spacing.unit * 3,
+              },
+              colors: {
+                primary: {
+                  main: "#368871"
+                }
+              }
+            }
+          }}
+        />
+      </div>
+    </Layout>
+  );
+}
+
+export default Reference;
