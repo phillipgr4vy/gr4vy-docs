96993af37d4259f0eb40d7bd814b84a3e5e1d3a3
Cristiano Betta <cristiano@betta.io>
2020-12-07 20:45:18 +0000

    Update image

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -5,7 +5,7 @@ module.exports = {
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
+  favicon: 'favicon.ico',
   organizationName: 'gr4vy',
   projectName: 'developer-documentation',
   stylesheets: [
