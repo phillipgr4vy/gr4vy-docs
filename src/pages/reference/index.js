d7cebf1df0c5beca88ce14d826997777f29e8845
Steve Nester <82510386+steve-gr4vy@users.noreply.github.com>
2021-05-19 14:40:08 +0100

    fix reference locally (#11)

--- src/pages/reference/index.js
+++ src/pages/reference/index.js
@@ -3,9 +3,11 @@ import Layout from "@theme/Layout"
 import Reference from "../../modules/Reference"
 
 const ReferencePage = () => {
+  const BASE_URL = process.env.BASE_URL || "/"
+  var url = BASE_URL + "openapi/openapi.v1.json";
   return (
     <Layout title="API Reference" >
-      <Reference specUrl={"/docs/openapi/openapi.v1.json"} />
+      <Reference specUrl={url} />
     </Layout>
   )
 }
