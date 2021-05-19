41adfb103c6487e8bd60722995f24164213310f4
Cristiano Betta <cbetta@gr4vy.com>
2021-05-19 15:29:58 +0100

    Update test suite (#14)

--- src/pages/reference/index.js
+++ src/pages/reference/index.js
@@ -2,14 +2,10 @@ import React from "react"
 import Layout from "@theme/Layout"
 import Reference from "../../modules/Reference"
 
-const ReferencePage = () => {
-  const BASE_URL = process.env.BASE_URL || "/"
-  var url = BASE_URL + "openapi/openapi.v1.json";
-  return (
-    <Layout title="API Reference" >
-      <Reference specUrl={url} />
-    </Layout>
-  )
-}
+const ReferencePage = () => (
+  <Layout title="API Reference" >
+    <Reference specUrl={"/openapi/openapi.v1.json"} />
+  </Layout>
+)
 
 export default ReferencePage
